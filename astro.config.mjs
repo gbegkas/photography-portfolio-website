67032a8be9b5b96aeff879c25a1d172d51c1075d
georgeb <georbegk10@gmail.com>
2026-08-22 22:28:21 +0300

    Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://gbegkas.github.io',
-  base: '/photography-portfolio-website',
+  site: 'https://www.light-lens.com',
   vite: {
     plugins: [tailwindcss()]
   }
